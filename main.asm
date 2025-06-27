b43a9dda80a03f657c5bf911e65f70467b9817f8
Sanda2 <santiagoaristimuno2005@gmail.com>
2025-06-27 06:59:27 -0400

    basic bipmap display
    algo basico y un poco de documentacion

--- main.asm
+++ main.asm
@@ -1,8 +1,10 @@
-#CAMBIO OBVIO PARA VER TRAKING
+#Tamaño del pixel 16*16
+#tamaño de la pantalla width 256, height 512
+# tamaño final de 16*32
 
 .data
     # Variables globales aquí
-    displayAddress: .word 0x10010000  # Dirección base del Bitmap Display
+display: .space 1024
 
 .text
 main:
@@ -12,10 +14,21 @@ main:
     j exit
 
 initialize_game:
+    li $t0 0x0000ff
+    li $t1 0
+	
     # Rutina para inicializar tableros, etc.
     jr $ra
 
 main_loop:
+    sw $t0 display($t1)
+    addi $t1 $t1 4
+    
+    beq $t1 2048 exit
+    
+    j main_loop
+
+
     # Lógica principal del juego
     jr $ra
 
